6a2e7b09651a04dc5b69fdee620f27b1cf70a324
btbu-causality <118962790+btbu-causality@users.noreply.github.com>
2022-12-03 00:06:57 +0800

    Add files via upload

---
+++ js/data/research.js
@@ -0,0 +1,25 @@
+let researchDomainList = [
+    {
+        '领域名称': '因果分析',
+        '描述': '<p><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; white-space: pre-wrap; background-color: #ffffff;">归因在生活、生产中无处不在。经典的归因方法有概率专家系统，如贝叶斯网络；归纳演绎法：从病例，医学文献中提取规则等。相较于经典的归因方法，因果归因解释了事件和行为发生的原因。我们主要研究因果归因，即分析果之因，后验的因之果等。</span></p>',
+    },
+    {
+        '领域名称': '因果效应估计',
+        '描述': '<p><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">因果推断在自然科学和人文社会科学中都有着重要的作用，是目前统计学中最具挑战性的研究方向之一。2021 年度诺贝尔经济学奖被授予三位美国经济学家 David Card, Joshua Angrist和Guido Imbens，以表彰他们在劳动经济学的实证研究和因果推断方法论上的贡献。因果网络图模型的提出者Judea Pearl也曾获得图灵奖。在当今大数据时代，利用统计学方法和机器学习方法从数据中挖掘分析因果作用越来越为重要。</span><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">在实际问题中，我们经常需要评估原因变量对结果变量的影响。例如，吸烟是否会导致肺癌，接受大学教育是否能够提高收入。随机化试验是评价因果作用的金标准，在随机化下，因果关系可以由相关关系来表示。但在临床研究中，随机化试验的开展经常需要高昂的成本，研究人员经常只能收集到观察性研究数据，因此，如何在此类问题的背景下评估因果作用尤为重要。对于观察性研究而言，基于相关性的分析往往不能得到正确的因果作用，从而导致了&ldquo;辛普森悖论&rdquo;等问题的出现。为了评估观察性研究中的因果作用，实验人员需要收集到原因和结果之间所有可能的混杂因素。通过分层调整所有的混杂，我们便能正确地评估此时的因果作用。在实际研究中, 如果有重要背景变量未被观测或被错误测量时, 就会存在未观测的混杂因素，此时需要引入额外的技术来识别和估计因果作用，例如工具变量以及阴性对照变量等等。</span><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">我们团队研究因果推断框架下的各类问题，包括缺失数据、测量误差、未观测混杂、以及选择偏差等等。</span></p>',
+    },
+    {
+        '领域名称': '因果关系发现',
+        '描述': '<p><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">与相关关系相比，因果关系在解释变量发生的因果机制和指导专家如何有效干预等场景有着无法取代的作用。近年来，通过分析观测数据来揭示变量间的因果关系，即因果发现（causal discovery），在人工智能和机器学习领域得到越来越多的关注。因果发现的核心在于&ldquo;推断变量间的因果网络&rdquo;，如区分睡眠问题与抑郁的因果关系。我们团队主要研究因果关系发现中的基础理论和应用方法，以从现实世界的复杂数据中发现因果网络。主要包含高维、隐变量、缺失值、测量误差和部分先验知识等的因果网络学习。</span></p>',
+    },
+    {
+        '领域名称': '因果推荐系统',
+        '描述': '<p><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">推荐系统是许多互联网公司的核心增长引擎，它旨在帮助用户发现对自己有价值的信息（物品），进行个性化推荐。基于机器学习/深度学习的推荐模型是当前推荐系统的主流方法，这类方法旨在更好地拟合用户行为数据，从而挖掘用户可能感兴趣的物品。然而，推荐系统收集到的用户行为数据往往是有偏差的，会受到用户的选择、系统的推荐、商品的内容或流行度等影响，因而观察到的用户行为数据往往不能够真实反应出用户的偏好，导致纯粹基于数据驱动的方法通常准确度和泛化能力低，缺乏鲁棒性和可解释性，难以取得预期效果。</span><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><br style="outline: none; margin: 0px; padding: 0px; font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;" /><span style="font-family: \'PingFang SC\', Helvetica, Tahoma, \'Hiragino Sans GB\', \'Microsoft YaHei\', SimSun, Heiti, sans-serif; font-size: 12px; text-align: center; white-space: pre-wrap; background-color: #ffffff;">推荐系统中的很多实际业务问题本质上都是因果问题。因果推荐系统旨在将因果推断技术与推荐系统相结合，去除推荐系统中存在的各种数据偏差问题，提高推荐模型的准确度、鲁棒性、泛化能力，改善推荐模型的公平性，可解释性，多样性，安全性等问题。因果推荐系统已逐渐成为推荐系统的一种研究趋势。我们主要研究推荐系统问题因果形式化，因果纠偏（选择偏差，混杂偏差，未观测混杂偏差，从众效应偏差），可解释推荐，隐反馈，延迟反馈，长期因果效应。</span></p>',
+    },
+];
+// for (let i = 0; i < 3; i++) {
+//     researchDomainList.push({
+//         id: i,
+//         '领域名称': '因果分析',
+//         '描述': '12321Three papers on causal discovery/causal fairness were accepted to NeurIPS 2022.',
+//     })
+// }
